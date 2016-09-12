30ce33b5afedb3265712952239a9d93c17ae5212
Glenn Holloway <holloway@cs50.harvard.edu>
2016-09-12 11:34:25 +0000

    v71: add -lcrypt flag and update caesar solution binary

--- ide50/files/etc/profile.d/ide50.sh
+++ ide50/files/etc/profile.d/ide50.sh
@@ -25,7 +25,7 @@ if [ "$(id -u)" != "0" ]; then
   # configure clang
   export CC=clang
   export CFLAGS="-ggdb3 -O0 -std=c11 -Wall -Werror -Wshadow"
-  export LDLIBS="-lcs50 -lm"
+  export LDLIBS="-lcs50 -lm -lcrypt"
 
   # protect user
   alias cp="cp -i"
