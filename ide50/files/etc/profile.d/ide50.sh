5b63c0d6d7a1a23c7bf4e1c43243f42d8ba169bc
David J. Malan <malan@harvard.edu>
2016-10-27 07:33:55 -0400

    replaces tabs with spaces in flask() function

--- ide50/files/etc/profile.d/ide50.sh
+++ ide50/files/etc/profile.d/ide50.sh
@@ -67,13 +67,12 @@ alias sqlite3="sqlite3 -column -header"
 # flask
 export FLASK_APP=application.py
 export FLASK_DEBUG=1
-
 function flask()
 {
     if [[ "$1" == "run" ]]; then
-	command flask run --host=0.0.0.0 --port=8080 --with-threads "${@:2}"
+        command flask run --host=0.0.0.0 --port=8080 --with-threads "${@:2}"
     else
-	command flask "$@"
+        command flask "$@"
     fi
 }
 
