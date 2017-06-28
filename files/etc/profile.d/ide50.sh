80a982772ef5d3a5a1aaf76d9b54adc1831a888f
David J. Malan <malan@harvard.edu>
2017-06-27 20:19:00 -0400

    added support for other options

--- files/etc/profile.d/ide50.sh
+++ files/etc/profile.d/ide50.sh
@@ -110,6 +110,11 @@ http-server()
             shift
             shift
         else
+            if [[ -z "$options" ]]; then
+                options="$1"
+            else
+                options+=" $1"
+            fi
             shift
         fi
     done
